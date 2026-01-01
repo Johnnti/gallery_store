b1dd74e563ad2b9dab9db25d48aeeb2c4c019380
JOHN NTI ANOKYE <john@Johns-MacBook-Air.local>
2026-01-01 16:48:51 -0600

    create image grid

--- frontend/src/Photo.tsx
+++ frontend/src/Photo.tsx
@@ -1,5 +1,9 @@
-export default function Photo() {
-    return (
-    <div className="border flex items-center justify-center">7</div>
-    )
+type PhotoProps = {
+    url: string;
 }
+
+export default function Photo({ url }: PhotoProps){
+    return (
+        <img src={url} alt="Photo" className="w-full h-64 object-cover"/>
+    )
+} 
--- frontend/src/PhotoSection.tsx
+++ frontend/src/PhotoSection.tsx
@@ -1,16 +1,21 @@
-import Photo from './photo.tsx'
-export default function PhotoSection() {
-    return (<>
-        <div className="absolute text-2xl top-0 left-0">Title</div>
-        <div className="w-full h-[20vh] grid grid-cols-4 grid-rows-4 border">
-            <Photo />
-            <Photo />
-            <Photo />
-            <Photo />
-            <Photo />
-            <Photo />
-        </div>
+import Photo from './Photo'
 
-        </>
+type ImageModule = { default: string }
+
+export default function PhotoSection() {
+    const imageModules = import.meta.glob<ImageModule>('./assets/*.jpg', { eager: true })
+    const images = Object.values(imageModules).map((mod) => mod.default)
+
+    return (
+        <div className="relative mx-auto p-4">
+            <h1 className="text-2xl">Title</h1>
+            <div className="grid grid-cols-1">
+                <div className="rounded-lg shadow-md">
+                {images.map((url, i) => (
+                    <Photo url={url} key={i} />
+                ))}
+                </div>
+            </div>
+        </div>
     )
 }
